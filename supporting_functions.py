98926916be2356fb19f038e3f2a9555205df90d0
Arindam Deka <arindamdeka001@gmail.com>
2026-02-02 19:07:10 +0530

    Update supporting_functions.py

--- supporting_functions.py
+++ supporting_functions.py
@@ -5,6 +5,7 @@ import re
 import streamlit as st
 
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import WebshareProxyConfig
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -32,7 +33,12 @@ def extract_video_id(url):
 
 # function to get transcript from the video.
 def get_transcript(video_id, language):
-    ytt_api= YouTubeTranscriptApi()
+    ytt_api= YouTubeTranscriptApi(
+        proxy_config=WebshareProxyConfig(
+            proxy_username="njvpuajz",
+            proxy_password="yn5821w9dv3h",
+        )
+    )
     try:
         transcript= ytt_api.fetch(video_id, languages=[language])
         full_transcript= " ".join([i.text for i in transcript])
@@ -179,5 +185,6 @@ def rag_answer(question, vectorstore):
     #chain
     chain = prompt|llm
     response= chain.invoke({"context":context_text,"question":question})
+
 
     return response.content
